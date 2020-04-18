f503a3a9099e41929fca9dcf931234dc3b2466a5
niccholaspage <niccholaspage@gmail.com>
2020-04-18 15:37:06 -0400

    first commit

---
+++ WordLadder/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello World!\n";
+
+    cout << "Argument 0: " << argv[0] << "\n";
+
+    return 0;
+}
