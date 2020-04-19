44c4d7d4b3dd127380112687b2dad33bf642aecc
niccholaspage <niccholaspage@gmail.com>
2020-04-19 18:54:56 -0400

    Check for proper argument count and print all arguments passed into program

--- WordLadder/Main.cpp
+++ WordLadder/Main.cpp
@@ -4,9 +4,22 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    if (argc < 4) {
+        cout << "Not enough arguments provided. Example usage:\n";
+        cout << "WordLadder dictionary_file_path first_word last_word\n";
+
+        return 0;
+    }
+
     cout << "Hello World!\n";
 
     cout << "Argument 0: " << argv[0] << "\n";
+
+    cout << "Argument 1 (name of dict file): " << argv[1] << "\n";
+
+    cout << "Argument 2 (first word in the ladder): " << argv[2] << "\n";
+
+    cout << "Argument 3 (last word in the ladder): " << argv[3] << "\n";
 
     return 0;
 }
