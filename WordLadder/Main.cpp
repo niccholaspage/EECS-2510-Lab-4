01f394cedce496cb69b7f0d0daae4d1a4c548703
niccholaspage <niccholaspage@gmail.com>
2020-04-21 18:57:42 -0400

    Work on comments in main method

--- WordLadder/Main.cpp
+++ WordLadder/Main.cpp
@@ -21,23 +21,38 @@ void convertToUppercase(string& str)
 
 int main(int argc, char* argv[])
 {
-	if (argc != 4)
+	// This is the entry point of the application, so we check
+	// our arguments to make sure they are correct, and attempt
+	// to get the word ladder between the starting and ending
+	// words the user provides, using the dictionary provided
+	// in the arguments.
+	//
+	if (argc != 4) // If our argument count isn't exactly four,
 	{
+		// Tell the user the wrong number of arguments was provided,
+		// and give them an example usage of the program so that
+		// they know how to use it!
+		//
 		cout << "Wrong number of arguments provided. Example usage:\n";
 		cout << "WordLadder dictionary_file_path first_word last_word\n";
 
-		return 0;
+		return 0; // There is nothing left to do, so we can return here, ending the program.
 	}
 
-	string dictionaryFilePath = argv[1];
-	string firstWord = argv[2];
-	string lastWord = argv[3];
+	string dictionaryFilePath = argv[1];	// Get the file path to the dictionary
+	string firstWord = argv[2];				// Get the word we are going to be starting at
+	string lastWord = argv[3];				// Get the word we will be ending at
 
-	if (firstWord.length() != lastWord.length())
+	if (firstWord.length() != lastWord.length()) // If the words aren't equal the same length,
 	{
+		// we tell the user that they don't have the same length.
+		// We can't go from the first word to the end word changing
+		// one letter at a time if they aren't the same length, as we
+		// are not allowed to add or remove characters!
+		//
 		cout << "The first and final word do not have the same length.\n";
 
-		return 0;
+		return 0; // We can't do anything with these word inputs, so we return, ending the program.
 	}
 
 	auto start = clock();
